7ebb61ad858956ad037cc00d55a5572bea811825
kauetrnt <rm559317@fiap.com.br>
2025-03-13 01:39:16 -0300

    Corrigindo alterações

--- sentinel/next.config.ts
+++ sentinel/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Gera o site estático automaticamente
-  images: {
-    unoptimized: true, // Evita erro de otimização de imagens no GitHub Pages
-  },
-  basePath: "/sentinel", // Substitua pelo nome real do repositório
+  /* config options here */
 };
 
 export default nextConfig;
