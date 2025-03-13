3a2f11b0446b6964b01001f1f2681618d3b8f4df
kauetrnt <rm559317@fiap.com.br>
2025-03-13 01:07:21 -0300

    Corrigir deploy para GitHub Pages

--- sentinel/next.config.ts
+++ sentinel/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Gera o site estático automaticamente
+  images: {
+    unoptimized: true, // Evita erro de otimização de imagens no GitHub Pages
+  },
+  basePath: "/sentinel", // Substitua pelo nome real do repositório
 };
 
 export default nextConfig;
