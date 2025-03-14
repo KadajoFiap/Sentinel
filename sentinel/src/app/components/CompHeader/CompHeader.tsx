fc30cabbe35384fca8318f6ed6b33026a5c89afe
kauetrnt <rm559317@fiap.com.br>
2025-03-14 01:56:36 -0300

    Add Link component to Header for logo navigation

--- sentinel/src/app/components/CompHeader/CompHeader.tsx
+++ sentinel/src/app/components/CompHeader/CompHeader.tsx
@@ -2,6 +2,7 @@
 
 import CompMenu from '../CompMenu/CompMenu';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
     return (
@@ -9,13 +10,15 @@ const Header = () => {
             <header className="fixed top-0 left-0 right-0 h-[60px] bg-[#f4f4f4] flex justify-between items-center px-5 shadow-md z-[1000]">
                 <CompMenu />
                 <div className="flex items-center relative z-[1001]">
-                    <Image
-                        src="/logoccr.png"
-                        alt="Logo CCR"
-                        width={55}
-                        height={0}
-                        priority
-                    />
+                    <Link href="/">
+                        <Image
+                            src="/logoccr.png"
+                            alt="Logo CCR"
+                            width={55}
+                            height={0}
+                            priority
+                        />
+                    </Link>
                 </div>
             </header>
         </>
