d29793c99a7af561ac0da07d76267e9696305dce
kauetrnt <rm559317@fiap.com.br>
2025-03-15 02:01:55 -0300

    Update Ocorrencias and Integrantes page auth

--- sentinel/src/app/Integrantes/page.tsx
+++ sentinel/src/app/Integrantes/page.tsx
@@ -1,8 +1,21 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import CompDavi from "../components/CompIntegrantes/CompDavi"
 import CompJoao from "../components/CompIntegrantes/CompJoao"
 import CompKaue from "../components/CompIntegrantes/CompKaue"
 
-const Integrantes = () =>{
+const Integrantes = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Verifica se o usuário está logado
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        if (!isLoggedIn) {
+            router.push('/Login');
+        }
+    }, [router]);
+
     return(
         <>
             <div className="min-h-screen bg-[#f4f4f4] pt-30 pl-10 pr-10 pb-30">
--- sentinel/src/app/Ocorrencias/page.tsx
+++ sentinel/src/app/Ocorrencias/page.tsx
@@ -1,6 +1,19 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import CompOcorrencias from "@/app/components/CompOcorrencias/CompOcorrencias";
 
-const Ocorrencias = () =>{
+const Ocorrencias = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Verifica se o usuário está logado
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        if (!isLoggedIn) {
+            router.push('/Login');
+        }
+    }, [router]);
+
     return(
         <>
             <CompOcorrencias />
