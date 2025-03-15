9d6fc3d5064dd524392ff010b525e6fb39a3c82e
kauetrnt <rm559317@fiap.com.br>
2025-03-15 02:26:32 -0300

    Revert "Update Next.js configuration: set output to 'export', enable unoptimized images, and define basePath for the Sentinel application."
    This reverts commit f9f8f228bc3b2f8b847d5e52f8f91c2bc45c6f87.

--- sentinel/next.config.ts
+++ sentinel/next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/sentinel'
 };
 
-module.exports = nextConfig;
+export default nextConfig;
